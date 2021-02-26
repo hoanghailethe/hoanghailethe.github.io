ea1979b4022213264ae57245c3c7285fbb832695
hoanghailethe <hoanghailethe@users.noreply.github.com>
2021-02-26 21:13:45 +0700

    Sua BTVN buoi 5

--- Java_basic/BT_day5/QuanLySinhVien/src/QuanLySV.java
+++ Java_basic/BT_day5/QuanLySinhVien/src/QuanLySV.java
@@ -1,14 +1,36 @@
 public class QuanLySV {
     public static void main(String[] args) throws Exception {
         System.out.println("Chào mừng đến với trang quán lý dữ liệu sinh viên !");
-        Student sv1 = new Student(11223344, "Hong Ngoc", 8.22f, 8.53f);
-        Student sv2 = new Student (22334455, "La Thuy", 9.4f, 7.5f);
-        Student sv3 = new Student (33445566, "Quang Huy", 10f, 8.5f);
-        Student sv4 = new Student();
+        System.out.println(" ");
+        Student sv1 = new Student();
+        sv1.hoTen = "Hong Ngoc";
+        sv1.maSinhVien = 123214214;
+        sv1.diemLyThuyet = 8.5f;
+        sv1.diemThucHanh = 9f;
+
+        Student sv2 = new Student();
+        sv2.hoTen = "Mai Lan";
+        sv2.maSinhVien = 12232143;
+        sv2.diemLyThuyet = 9.5f;
+        sv2.diemThucHanh = 9f;
+
+        Student sv3 = new Student();
+        sv3.hoTen = "Nguyen Huong";
+        sv3.maSinhVien = 13512314;
+        sv3.diemLyThuyet = 8.5f;
+        sv3.diemThucHanh = 10f;
+
+        Student sv4 = new Student(33445566, "Quang Huy", 10f, 8.5f);
+
+        System.out.printf("%7s %17s %20s %17s %n", "Ma SV", "Ho ten", "Diem Ly thuyet", "Diem thuc hanh");
+        sv1.display();
+        sv2.display();
+        sv3.display();
+        sv4.display();
 
         sv1.avrScoreCalculate();
         sv2.avrScoreCalculate();
         sv3.avrScoreCalculate();
-
+        sv4.avrScoreCalculate();
     }
 }
--- Java_basic/BT_day5/QuanLySinhVien/src/Student.java
+++ Java_basic/BT_day5/QuanLySinhVien/src/Student.java
@@ -6,22 +6,22 @@ public class Student {
     protected float diemTrungBinh;
 
     public Student() {
-        System.out.println("Một sinh viên vừa đăng ký tài khoản mặc đinh, không có thông tin");
-        this.hoTen = "Unknown";
-        this.diemLyThuyet = 0;
-        this.diemThucHanh = 0;
     }
 
-    public Student(int idStudent , String name, float testScore, float practiceScore ) {
-        System.out.println("Một sinh viên vừa đăng ký tài khoản chính thức, có cung cấp thông tin");
+    public Student(int idStudent, String name, float testScore, float practiceScore) {
         this.maSinhVien = idStudent;
         this.hoTen = name;
         this.diemLyThuyet = testScore;
         this.diemThucHanh = practiceScore;
     }
 
+    public void display() {
+        System.out.printf(" %3d\t %-15s %8.2f %18.2f %n", maSinhVien, hoTen, diemLyThuyet, diemThucHanh);
+    }
+
     public void avrScoreCalculate() {
-        this.diemTrungBinh = (diemLyThuyet+diemThucHanh)/2;
-        System.out.println("Điểm trung bình của học sinh có mã sinh viên "+ maSinhVien + " là " + this.diemTrungBinh );
+        this.diemTrungBinh = (diemLyThuyet + diemThucHanh) / 2;
+        System.out.println("Điểm trung bình của học sinh có tên " + this.hoTen + " và mã sinh viên " + maSinhVien
+                + " là " + this.diemTrungBinh);
     }
 }
