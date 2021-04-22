c703771569ea3d92e645a6b01502e33a2bc889ce
hoanghailethe <hoanghailethe@users.noreply.github.com>
2021-04-22 08:30:46 +0700

    update folder

---
+++ SpringBoot/Day2_Bean_HomeWork/SpringBoot.beanHW/src/main/java/vn/homeWork/Bean/TiviApplication.java
@@ -0,0 +1,11 @@
+package vn.homeWork.Bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiviApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiviApplication.class, args);
+    }
+}
