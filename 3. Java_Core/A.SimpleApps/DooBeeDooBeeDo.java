33048e5b4698f29c2376f583b09a8335b2440324
hoanghailethe <hoanghailethe@users.noreply.github.com>
2021-06-04 11:49:10 +0700

    add simple

---
+++ 3. Java_Core/A.SimpleApps/DooBeeDooBeeDo.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class DooBeeDooBeeDo {
+    public static void main(String[] args) {
+        int x=1;
+        while (x<3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        if (x==3) {
+            System.out.print("Do");
+        };
+    }
+}
